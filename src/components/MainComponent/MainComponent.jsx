32a5189021284a3c31a8fa960ce1647e1f1a5b0a
phyothukha <phyothukha2193@gmail.com>
2024-04-19 15:22:41 +0630

    feat:add comment

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
-const MainComponent = ({main}) => {
-  const {id,icon,desc,file}=main;
+const MainComponent = ({ main }) => {
+  const { id, icon, desc, file } = main;
   return (
-    <>   
-    
-    <form
-    // onSubmit={handleFormSubmit}
-   
-    className="duration-200 hover:bg-[#3A3B3C] cursor-pointer p-2 hover:rounded-lg mt-2 w-1/3 flex gap-2 justify-center items-center relative"
-  >
-    <img src={icon} alt="" />
-    <div className=" absolute opacity-0 left-2">
-    {file}
-    </div>
-    <p className=" text-white text-xs lg:text-base font-bold ">
-      {desc}
-    </p>
-  </form>
-  </>
+    <>
+      <form
+        // onSubmit={handleFormSubmit}
 
+        className="duration-200 hover:bg-[#3A3B3C] cursor-pointer p-2 hover:rounded-lg mt-2 w-1/3 flex gap-2 justify-center items-center relative"
+      >
+        <img src={icon} alt="" />
+        <div className=" absolute opacity-0 left-2">{file}</div>
+        <p className=" text-white text-xs lg:text-base font-bold ">{desc}</p>
+      </form>
+    </>
   );
 };
 
